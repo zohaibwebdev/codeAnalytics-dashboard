287607ce235f76d48cb8bab5a294cc89a456d874
unknown <zohaibfas@gmail.com>
2024-09-24 13:26:21 +0500

    feat: progress bar added in joining section

--- src/components/joining-section/JoiningSection.jsx
+++ src/components/joining-section/JoiningSection.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./JoiningSection.module.css";
 import Image from "next/image";
+
 const JoiningSection = () => {
+  const [progressWidth, setProgressWidth] = useState(40);
+
   return (
     <section className={styles.joiningSection}>
       <div className={styles.mainContainer}>
@@ -12,26 +16,34 @@ const JoiningSection = () => {
           <button className={styles.nextButton}>40% Complete</button>
         </div>
         <div className={styles.headingContainer}>
-            <h1>When do you need the developer to start?</h1>
-            <p>Decide when you will start your project together!</p>
+          <h1>When do you need the developer to start?</h1>
+          <p>Decide when you will start your project together!</p>
         </div>
       </div>
-      <div className={styles.joiningDiv}>
-        <div className={styles.panel}>
-            <Image src='/images/immediatly.png' width={60} height={60}/>
+      <div className={styles.spacer}>
+        <div className={styles.joiningDiv}>
+          <div className={styles.panel}>
+            <Image src="/images/immediatly.png" width={60} height={60} />
             <p>Immediately</p>
-        </div>
-        <div className={styles.panel}>
-            <Image src='/images/in-1-week.png' width={60} height={60}/>
+          </div>
+          <div className={styles.panel}>
+            <Image src="/images/in-1-week.png" width={60} height={60} />
             <p>In 1 to 2 weeks</p>
-        </div>
-        <div className={styles.panel}>
-            <Image src='/images/2-weeks.png' width={60} height={60}/>
+          </div>
+          <div className={styles.panel}>
+            <Image src="/images/2-weeks.png" width={60} height={60} />
             <p>&gt; 2 weeks from now</p>
-        </div>
-        <div className={styles.panel}>
-            <Image src='/images/not-sure.png' width={60} height={60}/>
+          </div>
+          <div className={styles.panel}>
+            <Image src="/images/not-sure.png" width={60} height={60} />
             <p>I am not sure</p>
+          </div>
+        </div>
+        <div className={styles.progress}>
+          <div
+            className={styles.progressBar}
+            style={{ width: `${progressWidth}%` }}
+          ></div>
         </div>
       </div>
     </section>
